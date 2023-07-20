eef04ce2baf1c1f8c527143cf9d8ab7e72400b15
Sarah Thomas <sbade92@gmail.com>
2023-07-19 20:32:36 -0700

    More styling and JS, updated images

--- js/main.js
+++ js/main.js
@@ -7,18 +7,36 @@ const color5 = document.querySelector("#color-swatch5");
 
 // Button Variables
 const aboutButton = document.querySelector("#about-me-button");
+const skillsButton = document.querySelector("#skills-button");
+const projectButton = document.querySelector("#project-button");
 
 // Div Variables
 const aboutSection = document.querySelector("#about-me-section");
+const skillsSection = document.querySelector("#skills-section");
+const projectSection = document.querySelector("#project-section");
 
 
 // Functions
 aboutButton.addEventListener("click", function(){
     if (aboutSection.classList.contains("hide")){
         aboutSection.classList.remove("hide");
-        color2.style.backgroundColor= '#88ff00';
     } else {
         aboutSection.classList.add("hide");
-        color2.style.backgroundColor= '#36bbb9';
+    }
+});
+
+skillsButton.addEventListener("click", function(){
+    if (skillsSection.classList.contains("hide")){
+        skillsSection.classList.remove("hide");
+    } else {
+        skillsSection.classList.add("hide");
+    }
+});
+
+projectButton.addEventListener("click", function(){
+    if (projectSection.classList.contains("hide")){
+        projectSection.classList.remove("hide");
+    } else {
+        projectSection.classList.add("hide");
     }
 });
